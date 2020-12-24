9a1be691f7667a3c4f3784aaaa32eb9542263bab
Stelios Kamnakis <kamnis1997@gmail.com>
2020-12-24 15:54:12 +0200

    Strapi Init, includes Auth Settings and Artist, Song models

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7da133e90a72e149a279d99d559feb5f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e8b40e21-e1d8-4f01-bf01-870c7e2c1022'
+};
