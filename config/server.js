f736bb0d679f2a5e3e2dd4166105227b7653656e
Stelios Kamnakis <kamnis1997@gmail.com>
2021-01-11 10:06:18 +0200

    Deploy changes

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env("URL", "http://192.168.1.58:1337"),
+  url: env("URL", "http://127.0.0.1:1337"),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '7da133e90a72e149a279d99d559feb5f'),
