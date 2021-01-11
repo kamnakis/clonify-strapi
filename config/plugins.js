cc1e0119e69f72c6d7a5ebf66acc139e7d16b4c0
Stelios Kamnakis <kamnis1997@gmail.com>
2021-01-11 16:37:58 +0200

    added cloudify provider for media library

--- config/plugins.js
+++ config/plugins.js
@@ -8,5 +8,13 @@ module.exports = ({ env }) => ({
       defaultFrom: 'kamnis1997@gmail.com',
       defaultReplyTo: 'kamnis1997@gmail.com'
     }
-  }
+  },
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
+    },
+  },
 })
